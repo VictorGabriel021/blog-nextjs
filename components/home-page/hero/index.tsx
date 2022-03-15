64d7f845abc4c1ac9f61018cf2f24d1d17a9e9b0
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2022-03-15 16:20:54 -0300

    feat: Adding global styles and folder structure

---
+++ components/home-page/hero/index.tsx
@@ -0,0 +1,16 @@
+import classes from "./styles.module.css";
+
+const Hero = () => {
+  return (
+    <section>
+      <div>Image</div>
+      <h1>Hi, I&apos;m Victor Gabriel</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
