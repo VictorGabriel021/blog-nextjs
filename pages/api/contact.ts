710069ea02bd5f147e4397f1cd9ca0edb59ec808
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2022-03-17 13:58:46 -0300

    feat: Adding user notification in contact form and sending data for mongoDB

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -16,21 +18,48 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
       return;
     }
 
+    type Props = {
+      id?: any;
+      email: any;
+      name: any;
+      message: any;
+    };
+
     // Strore it in a database
-    const newMessage = {
+    let newMessage: Props = {
       email,
       name,
       message,
     };
 
-    console.log(newMessage);
+    let client;
 
-    res
-      .status(201)
-      .json({
-        message: "Successfully stored message!",
-        newMessage: newMessage,
-      });
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://VictorGabriel021:VYcmnkv6yyvsqqo4@cluster0.f5euq.mongodb.net/myBlog?retryWrites=true&w=majority"
+      );
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
+
+    res.status(201).json({
+      message: "Successfully stored message!",
+      newMessage: newMessage,
+    });
   }
 };
 
