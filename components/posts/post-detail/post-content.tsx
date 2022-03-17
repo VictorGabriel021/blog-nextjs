b29e154e60953bebbc7234744c563e1baad82432
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2022-03-17 10:18:55 -0300

    refactor: Adding images and styling in code blocks into article

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -3,6 +3,10 @@ import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const DUMMY_POST = {
   slug: "getting-started-with-nextjs-1",
@@ -15,10 +19,55 @@ const DUMMY_POST = {
 const PostContent: React.FC<{ post: any }> = (props) => {
   const imagePath = `/images/posts/${props.post.slug}/${props.post.image}`;
 
+  const customRenderers = {
+    // img(image: any) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${props.post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph: any) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${props.post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+    code(code: any) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+
+      return (
+        <SyntaxHighlighter style={atomDark} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={props.post.title} image={imagePath} />
-      <ReactMarkdown>{props.post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>
+        {props.post.content}
+      </ReactMarkdown>
     </article>
   );
 };
